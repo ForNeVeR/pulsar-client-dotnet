87555e426ed67ab83718d3a123fc071f496d89b5
Ilyas Rahimov <rahimovir@gmail.com>
2020-04-20 23:36:09 +0500

    Move maxMessageSize to tls

--- tests/IntegrationTests/Basic.fs
+++ tests/IntegrationTests/Basic.fs
@@ -408,11 +408,14 @@ let tests =
             Log.Debug("Finished 'Scheduled message should be delivered at requested time'")
         }
         
+#if !NOTLS
         // Before running this test set 'maxMessageSize' for broker and 'nettyMaxFrameSizeBytes' for bookkeeper 
         testAsync "Send large message works fine" {
 
             Log.Debug("Started Send large message works fine")
-            let client = getClient()
+//            let client = getClient()
+            let client = getSslAdminClient()
+
             let topicName = "public/default/topic-" + Guid.NewGuid().ToString("N")
 
             let! producer =
@@ -449,5 +452,5 @@ let tests =
 
             Log.Debug("Finished Send large message works fine")
         }
-
+#endif
     ]
